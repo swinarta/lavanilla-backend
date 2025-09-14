dc2d81531578e2787179fa287771d0c30f6389d8
sas <806896+swinarta@users.noreply.github.com>
2025-09-14 23:29:44 +0800

    gh action

--- service/config.go
+++ service/config.go
@@ -2,7 +2,7 @@ package service
 
 const (
 	ShopId                   = "lvn-dev.myshopify.com"
-	AccessToken              = ""
+	AccessToken              = "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"
 	ShopifyGraphqlApiVersion = "2025-10"
 	HeaderKeyAccessToken     = "X-Shopify-Access-Token"
 	HeaderKeyUserAgent       = "User-Agent"
